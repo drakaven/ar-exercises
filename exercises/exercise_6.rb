8b84b19206e7b9ec54fa95cf8ac14e85db99ee45
matthew jenkins <drakaven@hotmail.com>
2017-02-14 21:23:23 +0000

    ex 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "funky", last_name: "Johnny", hourly_rate: 60)
+
+
+@store2 = Store.find(1)
+@store2.employees.create(first_name: "ff", last_name: "f", hourly_rate: 60)
+@store2.employees.create(first_name: "funky", last_name: "J", hourly_rate: 60)
+
+
+puts Employee.count
